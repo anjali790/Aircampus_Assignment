8d95c6a29ef8f359c6c96d4830baca6b1f39ad57
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-07 21:51:00 +0530

    assignment

---
+++ Assignment18(DOM-4-assign)/app.js
@@ -0,0 +1,35 @@
+const input=document.querySelector(".input");
+const btn=document.querySelector(".btn");
+const tbody=document.querySelector("tbody");
+const tbody2=document.querySelector("tbody2");
+const removebtn=document.querySelector("#delete");
+
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const newElm= document.createElement("tr");
+    newElm.setAttribute('class','row');
+    newElm.innerHTML = `
+    <td>${input.value}</td>
+    <td class="edit-btn"><button id="edit">\u{1F58A}</button></td>
+    <td class="delete-btn"><button id="delete">\u{1F5D1}</button></td>
+    `;
+    tbody.appendChild(newElm);
+});
+
+
+const removeRow=(e)=>{
+    if(e.target.id=='delete')
+    e.target.parentElement.parentElement.remove();
+}
+
+tbody.addEventListener('click',removeRow);
+
+
+
+const editRow=(e)=>{
+    if(e.target.id=='edit')
+    e.taget.parentElement.parentElement.contentEditable() =true;
+}
+ 
+tbody.addEventListener('click', editRow); 
