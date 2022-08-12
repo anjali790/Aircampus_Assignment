5e4976c65b97f7f1bfc972860287e6d8bbac9b24
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-12 14:23:36 +0530

    practice

--- Assignment18(DOM-4-assign)/app.js
+++ Assignment18(DOM-4-assign)/app.js
@@ -1,52 +1,54 @@
-const input=document.querySelector(".input");
-const btn=document.querySelector(".btn");
-const tbody=document.querySelector("tbody");
-const tbody2=document.querySelector("tbody2");
-const removebtn=document.querySelector("#delete");
-const check=document.querySelector("#check");
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const todo = document.querySelector('.todo');
+const todoIn = document.querySelector('.todo-in');
+const comp = document.querySelector('.complete');
+
+btn.addEventListener("click", function(){
+    setTimeout(function(){
+        input.value = "";
+    }, 1);
+
+    if (input.value.length > 0) {
+        let todoOuterDiv = document.createElement('div');
+        let check = document.createElement("input");
+        check.type = "checkbox";
+        let todoInput = document.createElement("input");
+        let button1 = document.createElement("button");
+        button1.innerText = "Edit";
+        button1.style.margin='5px';
+        let button2 = document.createElement("button");
+        button2.innerText = "Delete";
+        todoOuterDiv.append(check, todoInput, button1, button2);
+        todoIn.append(todoOuterDiv);
+
+        todoInput.value = input.value;
+        todoInput.setAttribute("readonly", "readonly");
 
 
-btn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const newElm= document.createElement("tr");
-    newElm.setAttribute('class','row');
-    newElm.innerHTML = `
-    <td class="check-box"><input type="checkbox" id="check">
-    <td id="data">${input.value}</td>
-    <td class="edit-btn"><button id="edit">\u{1F58A}</button></td>
-    <td class="delete-btn"><button id="delete">\u{1F5D1}</button></td>
-    `;
-    tbody.appendChild(newElm);
-});
+        button1.addEventListener('click', function(){
+            if (button1.innerText == "Edit") {
+                button1.innerText = "Save";
+                todoInput.removeAttribute('readonly');
+            } else {
+                button1.innerText = "Edit";
+                todoInput.setAttribute('readonly', 'readonly');
+            }
+        });
 
+        button2.addEventListener('click', function() {
+            button2.parentElement.remove();
+        });
 
-const removeRow=(e)=>{
-    if(e.target.id=='delete')
-    e.target.parentElement.parentElement.remove();
-}
+        let compDiv = document.createElement("div");
+        comp.append(compDiv);
 
-tbody.addEventListener('click',removeRow);
-
-
-const editRow=(e)=>{
-    if(e.target.id=='edit')
-    e.taget.input.value.contenteditable='true';
-    // e.taget.input.value.focus();
-    e.taget.input.value.innerText = "Save";
-}
- 
-tbody.addEventListener('click', editRow);
-
-
-check.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const compElm= document.createElement("tr");
-    compElm.setAttribute('class','row');
-    compElm.innerHTML = `
-    // <td class="check-box"><input type="checkbox" id="check">
-    <td id="data">${tbody.value}</td>
-    // <td class="edit-btn"><button id="edit">\u{1F58A}</button></td>
-    // <td class="delete-btn"><button id="delete">\u{1F5D1}</button></td>
-    `;
-    tbody2.appendChild(compElm);
+        check.addEventListener('click', function(){
+            check.parentElement.remove();
+            compDiv.append(todoOuterDiv);
+            button1.remove();
+            todoInput.style.border = "none";
+            todoInput.style.textDecoration = "line-through";
+        })
+    }
 });
